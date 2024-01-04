747e69ac196834770370c7ca041a05361d970c06
AbstractCJ <155411762+AbstractCJ@users.noreply.github.com>
2024-01-04 11:15:05 -0500

    Update AA.java

--- src/AA/AA.java
+++ src/AA/AA.java
@@ -6,7 +6,7 @@ public static void main(String[] args) {
 		
 		System.out.println("Need to write out the directions!!!.");
 		System.out.println("AA");
-		System.out.println("");
+		System.out.println("Finally!!!");
 	}
 
 }
