bb89f7c104eae23721abe4076fe63f06a349bd5d
User <User@DESKTOP-6JPNDEN.lan>
2024-01-04 11:09:17 -0500

    Directions

---
+++ src/AA/AA.java
@@ -0,0 +1,12 @@
+package AA;
+
+public class AA {
+	
+public static void main(String[] args) {
+		
+		System.out.println("Need to write out the directions!!!.");
+		System.out.println("AA");
+		System.out.println("");
+	}
+
+}
